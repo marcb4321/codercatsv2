100f6f52071943bb6b8a2d9d7a8df36af4c215e8
Marc <bmarc4321@gmail.com>
2019-04-06 19:34:47 +0100

    Added date time processor copyright symbol (should print current year and update automatically).

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def say_hello():
   return render_template("index.html")
+
+from datetime import datetime
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
 
 app.run(debug=True)
