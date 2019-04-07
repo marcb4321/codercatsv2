84bca8ffe3f4699b3dcfe6781f2aa574b4e6107a
Marc <bmarc4321@gmail.com>
2019-04-07 01:26:06 +0100

    Added more links to anchors

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 
 app = Flask(__name__)
@@ -6,6 +6,29 @@ app = Flask(__name__)
 @app.route("/")
 def say_hello():
   return render_template("index.html")
+
+
+@app.route("/loginregister")
+def loginregister():
+    return render_template("loginregister.html")
+
+
+
+@app.route('/whatiscodercats')
+def whatiscodercats():
+    return redirect(url_for('say_hello',_anchor='whatiscodercats'))
+
+@app.route('/howitworks')
+def howitworks():
+    return redirect(url_for('say_hello',_anchor='howitworks'))
+
+@app.route('/tryitnow')
+def tryitnow():
+    return redirect(url_for('say_hello',_anchor='tryitnow'))
+
+@app.route('/contact')
+def contact():
+    return redirect(url_for('say_hello',_anchor='contact'))
 
 from datetime import datetime
 @app.context_processor
