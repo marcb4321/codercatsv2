fb3e4f0400e36d824eeae393a1d88e849803fa49
Marc <bmarc4321@gmail.com>
2019-05-18 00:49:09 +0100

    Added link to register page

--- app.py
+++ app.py
@@ -12,6 +12,10 @@ def home():
 def login():
     return render_template("login.html")
 
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 @app.route("/tryitnowpg")
 def tryitnowpg():
     return render_template("tryitnow.html")
